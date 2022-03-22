f727676d655ff4afd97838eb0e35b970803325db
Grace Wang <wangxiaotian2012@gmail.com>
2022-03-22 04:04:40 +0000

    Added a route to redirect "/u/:shortURL" to its longURL

--- express_server.js
+++ express_server.js
@@ -39,11 +39,14 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-
-
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 app.get("/urls.json", (req, res) => {
