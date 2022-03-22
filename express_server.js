abf494a87f45288a354840469614ab1334b2cb1e
Grace Wang <wangxiaotian2012@gmail.com>
2022-03-22 00:48:22 +0000

    Included the header partial inside urls_index.ejs and urls_show.ejs

--- express_server.js
+++ express_server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -14,7 +14,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urlDatabase }
+  const templateVars = { urlDatabase };
   res.render("urls_index", templateVars);
 });
 
