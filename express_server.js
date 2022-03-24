2ddbc67e92e8a3d96d436feb871c825ff53cc354
Grace Wang <wangxiaotian2012@gmail.com>
2022-03-24 02:07:06 +0000

    Changed urlDatabase structure

--- express_server.js
+++ express_server.js
@@ -12,8 +12,14 @@ app.use(cookieParser());
 
 // Database
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2": {
+    longURL: "http://www.lighthouselabs.ca",
+    userID: "admin"
+  },
+  "9sm5xK": {
+    longURL: "http://www.google.com",
+    userID: "admin"
+  }
 };
 
 const users = {
